66b8a41a76a3f0cac599c668241da5c69bd25f3d
Rando Wiltschek <randomw@gmx.net>
2017-10-14 19:11:14 +0200

    Cleaned up guns and fixed flamethrower

---
+++ Source/RimWorld_Mechanoids/Convertable/Projectile_Ignite.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MoreMechanoids
+{
+    public class Projectile_Ignite : Projectile
+    {
+        protected override void Impact(Thing hitThing)
+        {
+            Ignite();
+        }
+
+        protected virtual void Ignite()
+        {
+            Map map = Map;
+            Destroy();
+            //ThingDef spawnThingDef = def.projectile.preExplosionSpawnThingDef;
+            float ignitionChance = def.projectile.explosionSpawnChance;
+            var radius = def.projectile.explosionRadius;
+            var cellsToAffect = SimplePool<List<IntVec3>>.Get();
+            cellsToAffect.Clear();
+            cellsToAffect.AddRange(def.projectile.damageDef.Worker.ExplosionCellsToHit(Position, map, radius));
+
+            MoteMaker.MakeStaticMote(Position, map, ThingDefOf.Mote_ExplosionFlash, radius*6f);
+            for (int i = 0; i < 4; i++)
+            {
+                MoteMaker.ThrowSmoke(Position.ToVector3Shifted() + Gen.RandomHorizontalVector(radius*0.7f), map, radius*0.6f);
+            }
+
+            if (Rand.Chance(ignitionChance))
+                foreach (var vec3 in cellsToAffect)
+                {
+                    var fireSize = radius-vec3.DistanceTo(Position);
+                    if (fireSize > 0.1f)
+                    {
+                        FireUtility.TryStartFireIn(vec3, map, fireSize);
+                    }
+                }
+        }
+
+        public override Quaternion ExactRotation
+        {
+            get
+            {
+                var forward = destination - origin;
+                forward.y = 0;
+                return Quaternion.LookRotation(forward);
+            }
+        }
+    }
+}
