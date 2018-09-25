0049224f4823244ce9912bb13674e7a3bdfde354
Roel <roelheirman@gmail.com>
2018-09-25 10:21:28 +0200

    only use downallhumans when not playerfaction

---
+++ Source/RimWorld_Mechanoids/NewMechanoids/ThinkNode_ConditionalNonPlayerFaction.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace MoreMechanoids
+{
+    class ThinkNode_ConditionalNonPlayerFaction : ThinkNode_Conditional
+    {
+        protected override bool Satisfied(Pawn pawn)
+        {
+            return pawn.Faction != null && pawn.Faction != Faction.OfPlayer;
+        }
+    }
+}
