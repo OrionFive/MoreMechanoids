ccce298f9a69b060e49c8f486202433827b230b7
Rando Wiltschek <randomw@gmx.net>
2020-03-08 13:10:42 +0100

    - Fix: Skullywags now sleep when dormant and attack doors correctly again

---
+++ Source/RimWorld_Mechanoids/Mechanoids/JobGiver_AIOpenDoors.cs
@@ -0,0 +1,53 @@
+using System;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace MoreMechanoids {
+    public class JobGiver_AIOpenDoors : ThinkNode_JobGiver
+    {
+        private const int CloseSearchRadius = 56;
+        private static Predicate<Thing> validDoor = t => t is Building_Door door && !door.Destroyed && !door.Open;
+        private static IntRange expiryInterval = new IntRange(450, 500);
+
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            if (!pawn.HostileTo(Faction.OfPlayer)) return null;
+            
+            var door = FindNearbyDoor(pawn, CloseSearchRadius, validDoor);
+
+            if (door == null) return null;
+
+            return CreateJob(pawn, door);
+        }
+
+        private static Job CreateJob(Pawn pawn, Building_Door door)
+        {
+            foreach (var verb in pawn.VerbTracker.AllVerbs.InRandomOrder())
+            {
+                if(verb.Available() && verb.IsUsableOn(door) && !door.Open)
+                {
+                    Job job = JobMaker.MakeJob(JobDefOf.UseVerbOnThing, door);
+                    job.verbToUse = verb;
+                    job.expiryInterval = expiryInterval.RandomInRange;
+                    job.checkOverrideOnExpire = true;
+                    job.expireRequiresEnemiesNearby = true;
+                    job.attackDoorIfTargetLost = true;
+                    return job;
+                }
+            }
+            return null;
+        }
+
+        private static Building_Door FindNearbyDoor(Pawn pawn, float maxDist, Predicate<Thing> validator)
+        {
+            return GenClosest.ClosestThing_Global_Reachable(pawn.Position, pawn.Map, pawn.Map.listerBuildings.AllBuildingsColonistOfClass<Building_Door>(), PathEndMode.Touch, TraverseParms.For(pawn), maxDist, validator) as Building_Door;
+        }
+
+        private bool IsDoorInRange(Thing thing, Thing searcherThing, IntVec3 flagPosition, double radiusSquared)
+        {
+            return (thing.Position - flagPosition).LengthHorizontalSquared <= radiusSquared && searcherThing.HostileTo(thing) && validDoor(thing);
+        }
+
+    }
+}
--- Source/RimWorld_Mechanoids/Mechanoids/JobGiver_DownAllHumans.cs
+++ Source/RimWorld_Mechanoids/Mechanoids/JobGiver_DownAllHumans.cs
@@ -1,85 +1,31 @@
 using System;
-using System.Linq;
-using HarmonyLib;
 using RimWorld;
 using Verse;
 using Verse.AI;
-using Verse.AI.Group;
 
 namespace MoreMechanoids
 {
     public class JobGiver_DownAllHumans : JobGiver_AIFightEnemies
     {
-        public float targetKeepRadius = 72f;
-        public float targetAcquireRadius = 65f;
-        private Predicate<Thing> validDoor = t => t is Building_Door door && !door.Destroyed && !door.Open;
-        private Predicate<Thing> validPawn = t => t is Pawn pawn && !pawn.Destroyed && !pawn.Downed && pawn.def.race != null && pawn.def.race.IsFlesh;
+        public const float targetKeepRadius = 65;
+        public const float targetAcquireRadius = 56;
+        private readonly Predicate<Thing> validPawn = t => t is Pawn pawn && !pawn.Destroyed && !pawn.Downed && pawn.def.race != null && pawn.def.race.IsFlesh;
 
         protected override bool ExtraTargetValidator(Pawn pawn, Thing target)
         {
-            return validDoor(target) || validPawn(target);
+            return validPawn(target);
         }
 
-        protected override void UpdateEnemyTarget(Pawn pawn)
+        protected override Thing FindAttackTarget(Pawn pawn)
         {
-            if (pawn == null) return;
-            var thing = pawn.mindState.enemyTarget;
-            if (thing != null)
-            {
-                //Log.Message($"{pawn.Label}: was going for {thing.Label}.");
-                if (thing.Destroyed || Find.TickManager.TicksGame - pawn.mindState.lastEngageTargetTick > 400 || !pawn.CanReach(thing, PathEndMode.Touch, Danger.Deadly, true)
-                    || (pawn.Position - thing.Position).LengthHorizontalSquared > targetKeepRadius*targetKeepRadius)
-                {
-                    thing = null;
-                }
+            TargetScanFlags flags = TargetScanFlags.NeedLOSToPawns | TargetScanFlags.NeedReachableIfCantHitFromMyPos | TargetScanFlags.NeedThreat | TargetScanFlags.NeedAutoTargetable;
+            //if (this.needLOSToAcquireNonPawnTargets)
+            //    flags |= TargetScanFlags.NeedLOSToNonPawns;
+            //if (this.PrimaryVerbIsIncendiary(pawn))
+            //    flags |= TargetScanFlags.NeedNonBurning;
 
-                if (thing is Pawn pawn2 && pawn2.Downed)
-                {
-                    thing = null;
-                }
-
-                if (thing is Building_Door door && door.Open)
-                {
-                    thing = null;
-                }
-            }
-
-            if (thing == null)
-            {
-                const TargetScanFlags targetScanFlags = TargetScanFlags.NeedReachable;
-
-                thing = AttackTargetFinder.BestAttackTarget(pawn, targetScanFlags, validPawn, 0f, targetAcquireRadius) as Thing;
-                if (thing == null)
-                {
-                    //Thing thing2 = GenAI.BestAttackTarget(pawn.Position, pawn, validatorDoor, targetAcquireRadius, 0f, targetScanFlags2);
-                    var door = pawn.Map.listerBuildings.AllBuildingsColonistOfClass<Building_Door>()
-                            .Where(b => validDoor(b) && pawn.Map.reachability.CanReach(b.Position, pawn.Position, PathEndMode.Touch, TraverseMode.PassDoors, Danger.Deadly))
-                            .OrderBy(t => t.Position.DistanceToSquared(pawn.Position))
-                            .FirstOrDefault();
-
-                    if (door != null)
-                    {
-                        thing = door;
-                    }
-                }
-                if (thing != null && thing != pawn.mindState.enemyTarget)
-                {
-                    //Log.Message(pawn.LabelShort + " attacking " + thing.LabelShort + " at " + thing.Position);
-                    Traverse.Create(pawn.mindState).Method("Notify_EngagedTarget");
-
-                    Lord lord = pawn.Map.lordManager.LordOf(pawn);
-
-                    lord?.Notify_PawnAcquiredTarget(pawn, thing);
-
-                    pawn.CurJob?.SetTarget(TargetIndex.A, thing);
-
-                    pawn.mindState.enemyTarget = thing;
-                }
-                if(thing == null)
-                {
-                    pawn.mindState.enemyTarget = null;
-                }
-            }
+            var attackTarget = (Thing) AttackTargetFinder.BestAttackTarget(pawn, flags, x => ExtraTargetValidator(pawn, x), 0.0f, targetAcquireRadius, GetFlagPosition(pawn), GetFlagRadius(pawn));
+            return attackTarget;
         }
     }
 }
