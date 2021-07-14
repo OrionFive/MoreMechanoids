37ea1a506f0183fb6dc6b03a179cad0772373c53
OrionFive <wiltschek@gmx.net>
2021-07-14 16:00:13 +0200

    - New: Flamebots can now be sappers too

--- Source/RimWorld_Mechanoids/Harmony/RaidStrategyWorker_ImmediateAttackSappers_Patch.cs
+++ Source/RimWorld_Mechanoids/Harmony/RaidStrategyWorker_ImmediateAttackSappers_Patch.cs
@@ -4,24 +4,28 @@ using Verse;
 
 namespace MoreMechanoids.Harmony
 {
-    // Sappers aren't a thing anymore. Breaching? Gotta test it first.
-    ///// <summary>
-    ///// Don't spawn Mechanoid sapper attacks all the time when wealth is high
-    ///// </summary>
-    //public class RaidStrategyWorker_ImmediateAttackSappers_Patch
-    //{
-    //    [HarmonyPatch(typeof(RaidStrategyWorker_ImmediateAttackSappers), nameof(RaidStrategyWorker.CanUseWith))]
-    //    public class CanUseWith
-    //    {
-    //        [HarmonyPostfix]
-    //        internal static void Postfix(IncidentParms parms, ref bool __result)
-    //        {
-    //            if (!__result) return;
-    //            if (parms.faction != Faction.OfMechanoids) return;
-    //
-    //            // Only do sappers 15% of the time
-    //            __result = Rand.ChanceSeeded(0.15f, GenMath.RoundRandom(parms.points));
-    //        }
-    //    }
-    //}
+    /// <summary>
+    /// Don't spawn Mechanoid sapper attacks all the time when wealth is high
+    /// </summary>
+    public class RaidStrategyWorker_ImmediateAttackSappers_Patch
+    {
+        [HarmonyPatch(typeof(RaidStrategyWorker_ImmediateAttackSappers), nameof(RaidStrategyWorker_ImmediateAttackSappers.CanUsePawn))]
+        public class CanUsePawn
+        {
+            internal static void Postfix(float pointsTotal, Pawn p, ref bool __result)
+            {
+                if (!__result) return;
+                if (p?.def.defName == "Mech_Mammoth")
+                {
+                    // Only can be sappers 15% of the time; use points as seed, so same attack acts the same way
+                    __result = Rand.ChanceSeeded(0.15f, GenMath.RoundRandom(pointsTotal));
+                }
+                if (p?.def.defName == "Mech_Flamebot")
+                {
+                    // Only can be sappers 20% of the time; use points as seed, so same attack acts the same way
+                    __result = Rand.ChanceSeeded(0.20f, GenMath.RoundRandom(pointsTotal)+1);
+                }
+            }
+        }
+    }
 }
