cfc0ec2f15b22003e181aa41780cd20012ae9dcc
OrionFive <wiltschek@gmx.net>
2021-07-14 15:17:07 +0200

    Cleanup

--- Source/RimWorld_Mechanoids/Harmony/RaidStrategyWorker_ImmediateAttackSappers_Patch.cs
+++ Source/RimWorld_Mechanoids/Harmony/RaidStrategyWorker_ImmediateAttackSappers_Patch.cs
@@ -4,23 +4,24 @@ using Verse;
 
 namespace MoreMechanoids.Harmony
 {
-    /// <summary>
-    /// Don't spawn Mechanoid sapper attacks all the time when wealth is high
-    /// </summary>
-    public class RaidStrategyWorker_ImmediateAttackSappers_Patch
-    {
-        [HarmonyPatch(typeof(RaidStrategyWorker_ImmediateAttackSappers), nameof(RaidStrategyWorker_ImmediateAttackSappers.CanUseWith))]
-        public class CanUseWith
-        {
-            [HarmonyPostfix]
-            internal static void Postfix(IncidentParms parms, ref bool __result)
-            {
-                if (!__result) return;
-                if (parms.faction != Faction.OfMechanoids) return;
-
-                // Only do sappers 15% of the time
-                __result = Rand.ChanceSeeded(0.15f, GenMath.RoundRandom(parms.points));
-            }
-        }
-    }
+    // Sappers aren't a thing anymore. Breaching? Gotta test it first.
+    ///// <summary>
+    ///// Don't spawn Mechanoid sapper attacks all the time when wealth is high
+    ///// </summary>
+    //public class RaidStrategyWorker_ImmediateAttackSappers_Patch
+    //{
+    //    [HarmonyPatch(typeof(RaidStrategyWorker_ImmediateAttackSappers), nameof(RaidStrategyWorker.CanUseWith))]
+    //    public class CanUseWith
+    //    {
+    //        [HarmonyPostfix]
+    //        internal static void Postfix(IncidentParms parms, ref bool __result)
+    //        {
+    //            if (!__result) return;
+    //            if (parms.faction != Faction.OfMechanoids) return;
+    //
+    //            // Only do sappers 15% of the time
+    //            __result = Rand.ChanceSeeded(0.15f, GenMath.RoundRandom(parms.points));
+    //        }
+    //    }
+    //}
 }
